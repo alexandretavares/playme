ced26fffa124458485ae51766cef34713bb0e5d2
Alexandre Tavares <tm.alexandre@gmail.com>
2016-10-17 23:35:42 -0300

    Menu backdrop adicionado. Novas imagens dos albums. Correções no layout.

--- www/app/album/album.value.js
+++ www/app/album/album.value.js
@@ -3,74 +3,74 @@
 
     angular.module("playme.album").value("albumList", [
         {
-            name: "Apocalyptica",
-            band: "Metalica",
+            name: "Someday",
+            band: "Nickelback",
             duration: "20:35",
             rating: 3,
-            image: 'img/album.jpg'
+            image: 'img/album1.jpg'
         },
         {
             name: "Equilibrium",
             band: "Sagas",
             duration: "31:27",
             rating: 0,
-            image: 'img/album.jpg'
+            image: 'img/album2.jpg'
         },
         {
             name: "In Extremo",
             band: "Sterneisen",
             duration: "42:30",
             rating: 2,
-            image: 'img/album.jpg'
+            image: 'img/album3.jpg'
         },
         {
             name: "Apocalyptica",
             band: "Wagner Reloaded",
             duration: "12:45",
             rating: 4,
-            image: 'img/album.jpg'
+            image: 'img/album4.jpg'
         },
         {
-            name: "Apocalyptica",
-            band: "Metalica",
+            name: "Dark Horse",
+            band: "Nickelback",
             duration: "20:35",
             rating: 3,
-            image: 'img/album.jpg'
+            image: 'img/album1.jpg'
         },
         {
             name: "Equilibrium",
             band: "Sagas",
             duration: "31:27",
             rating: 0,
-            image: 'img/album.jpg'
+            image: 'img/album2.jpg'
         },
         {
             name: "In Extremo",
             band: "Sterneisen",
             duration: "42:30",
             rating: 2,
-            image: 'img/album.jpg'
+            image: 'img/album1.jpg'
         },
         {
             name: "Apocalyptica",
             band: "Wagner Reloaded",
             duration: "12:45",
             rating: 4,
-            image: 'img/album.jpg'
+            image: 'img/album3.jpg'
         },
         {
             name: "Apocalyptica",
             band: "Metalica",
             duration: "20:35",
             rating: 3,
-            image: 'img/album.jpg'
+            image: 'img/album4.jpg'
         },
         {
             name: "Equilibrium",
             band: "Sagas",
             duration: "31:27",
             rating: 0,
-            image: 'img/album.jpg'
+            image: 'img/album1.jpg'
         }
     ]);
 
